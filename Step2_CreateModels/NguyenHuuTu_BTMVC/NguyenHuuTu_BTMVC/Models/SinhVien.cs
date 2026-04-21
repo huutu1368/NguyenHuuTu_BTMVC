430d655c33c063ed163d3d69f5d99cad66d531f6
Huu Tu <huutu1368@gmail.com>
2026-04-21 20:05:32 +0700

    NguyenHuuTu_Step2

---
+++ Step2_CreateModels/NguyenHuuTu_BTMVC/NguyenHuuTu_BTMVC/Models/SinhVien.cs
@@ -0,0 +1,9 @@
+﻿namespace NguyenHuuTu_BTMVC.Models
+{
+    public class SinhVien
+    {
+        public int SinhVienId { get; set; }
+        public string tenSinhVien { get; set; }
+        public ICollection<DangKyLopHoc> dangKyLopHocs { get; set; }
+    }
+}
