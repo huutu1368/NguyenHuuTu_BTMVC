a4617ba8b72f074af695e687e3ec604276372ad0
Huu Tu <huutu1368@gmail.com>
2026-04-21 20:36:26 +0700

    NguyenHuuTu_Step5

---
+++ Step5_CreateControllerAndView/NguyenHuuTu_BTMVC/NguyenHuuTu_BTMVC/Controllers/GiaoViensController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NguyenHuuTu_BTMVC.Models;
+
+namespace NguyenHuuTu_BTMVC.Controllers
+{
+    public class GiaoViensController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public GiaoViensController(AppDbContext db) => _db = db;
+        public IActionResult Index() => View(_db.giaoViens.ToList());
+        public IActionResult Create() => View();
+        [HttpPost]
+        public IActionResult Create(GiaoVien gv)
+        {
+            _db.giaoViens.Add(gv);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id) => View(_db.giaoViens.Find(id));
+        [HttpPost]
+        public IActionResult Edit(GiaoVien gv)
+        {
+            _db.giaoViens.Update(gv);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            var gv = _db.giaoViens.Find(id);
+            if (gv != null)
+            {
+                _db.giaoViens.Remove(gv);
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Step5_CreateControllerAndView/NguyenHuuTu_BTMVC/NguyenHuuTu_BTMVC/Controllers/KhoaHocsController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NguyenHuuTu_BTMVC.Models;
+
+namespace NguyenHuuTu_BTMVC.Controllers
+{
+    public class KhoaHocsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public KhoaHocsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: KhoaHocs
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.khoaHocs.ToListAsync());
+        }
+
+        // GET: KhoaHocs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var khoaHoc = await _context.khoaHocs
+                .FirstOrDefaultAsync(m => m.KhoaHocId == id);
+            if (khoaHoc == null)
+            {
+                return NotFound();
+            }
+
+            return View(khoaHoc);
+        }
+
+        // GET: KhoaHocs/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: KhoaHocs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("KhoaHocId,tenKH")] KhoaHoc khoaHoc)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(khoaHoc);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(khoaHoc);
+        }
+
+        // GET: KhoaHocs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var khoaHoc = await _context.khoaHocs.FindAsync(id);
+            if (khoaHoc == null)
+            {
+                return NotFound();
+            }
+            return View(khoaHoc);
+        }
+
+        // POST: KhoaHocs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("KhoaHocId,tenKH")] KhoaHoc khoaHoc)
+        {
+            if (id != khoaHoc.KhoaHocId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(khoaHoc);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!KhoaHocExists(khoaHoc.KhoaHocId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(khoaHoc);
+        }
+
+        // GET: KhoaHocs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var khoaHoc = await _context.khoaHocs
+                .FirstOrDefaultAsync(m => m.KhoaHocId == id);
+            if (khoaHoc == null)
+            {
+                return NotFound();
+            }
+
+            return View(khoaHoc);
+        }
+
+        // POST: KhoaHocs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var khoaHoc = await _context.khoaHocs.FindAsync(id);
+            if (khoaHoc != null)
+            {
+                _context.khoaHocs.Remove(khoaHoc);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool KhoaHocExists(int id)
+        {
+            return _context.khoaHocs.Any(e => e.KhoaHocId == id);
+        }
+    }
+}
